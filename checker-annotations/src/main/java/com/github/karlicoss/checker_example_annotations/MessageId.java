d611eac6fb9d6a3d46060671c681775bbf25575a
Dima Gerasimov <karlicoss@gmail.com>
2016-12-07 15:19:57 +0300

    Subproject with custom Fenum annotations (#3)

---
+++ checker-annotations/src/main/java/com/github/karlicoss/checker_example_annotations/MessageId.java
@@ -0,0 +1,17 @@
+package com.github.karlicoss.checker_example_annotations;
+
+
+import org.checkerframework.checker.fenum.qual.FenumTop;
+import org.checkerframework.framework.qual.SubtypeOf;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@SubtypeOf(FenumTop.class)
+public @interface MessageId {}
